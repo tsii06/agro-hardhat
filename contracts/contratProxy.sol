2d76599a6db6cc69e9fc6894c31ea78d3beb6327
vgeeky <valyandrianimpanana@gmail.com>
2025-02-28 15:00:43 +0300

    [FIX] supprime "ContratProxy copy.sol"

--- contracts/contratProxy.sol
+++ contracts/contratProxy.sol
@@ -17,12 +17,14 @@ contract ContratProxy {
         require(impl != address(0), "Implementation contract not set");
 
         assembly {
-            let ptr := mload(0x40)
-            calldatacopy(ptr, 0, calldatasize())
-            let result := delegatecall(gas(), impl, ptr, calldatasize(), 0, 0)
-            let size := returndatasize()
-            returndatacopy(ptr, 0, size)
+            let ptr := mload(0x40) // allouer de l'espace memoire pour l'appel.
+            calldatacopy(ptr, 0, calldatasize()) // Copier les donnees de l'appel dans la memoire.
 
+            let result := delegatecall(gas(), impl, ptr, calldatasize(), 0, 0) // appel l'implementation via delegatecall
+            let size := returndatasize()
+            returndatacopy(ptr, 0, size) // copier la reponse de l'appel
+
+            // retourne ou annule l'execution selon le resultat
             switch result
             case 0 { revert(ptr, size) }
             default { return(ptr, size) }
